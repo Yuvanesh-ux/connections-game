376314fc6192979cda17d4cb7558c50fd7f0b3cd
Yuvanesh-ux <pilotyuvanesh@gmail.com>
2025-02-09 10:25:58 -0500

    gh-pages

--- connections-game/next.config.mjs
+++ connections-game/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/connections-game/' : '',
+  basePath: isProd ? '/connections-game' : '',
+  output: 'export'
+};
 
 export default nextConfig;
